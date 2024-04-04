13cfc8f68055e27ca54caae1d62d218cace84224
Evan <suzukaze.hazuki2020@gmail.com>
2024-04-04 18:00:58 +0900

    fix js config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app:{
+    baseURL:'/nuxt-flowbite-samples/'
+  },
   modules: [
     "@pinia/nuxt",
     "@nuxtjs/tailwindcss",
